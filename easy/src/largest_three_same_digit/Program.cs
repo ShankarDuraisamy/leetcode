5116e4dc075ecc0a685a7e995eb27db407d9e079
ShankarDuraisamy <51428949+ShankarDuraisamy@users.noreply.github.com>
2023-12-05 21:47:07 +0530

    Add files via upload

---
+++ easy/src/largest_three_same_digit/Program.cs
@@ -0,0 +1,18 @@
+﻿
+class SlidingWindowTechnique
+{
+    public string LargestGoodInteger(string num)
+    {
+        int first_ptr = 0, last_ptr = 0;
+        double result = -1;
+        while (last_ptr < num.Length)
+        {
+            while (last_ptr < num.Length && num[first_ptr] == num[last_ptr])
+                last_ptr++;
+            if ((last_ptr - 1) - first_ptr >= 2)
+                result = result > Char.GetNumericValue(num[first_ptr]) ? result : Char.GetNumericValue(num[first_ptr]);
+            first_ptr = last_ptr++;
+        }
+        return result == -1 ? "" : $"{result}{result}{result}";
+    }
+}
